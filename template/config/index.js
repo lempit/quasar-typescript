aa7cb623e8e66b26cafbf17f30c71fb5e2495ab8
budiadiono <budiadiono@gmail.com>
2017-09-07 10:27:19 +0700

    Oops!

--- template/config/index.js
+++ template/config/index.js
@@ -33,7 +33,7 @@ module.exports = {
     // auto open browser or not
     openBrowser: true,
     publicPath: '/',
-    port: 8080,
+    port: {{port}},
 
     // If for example you are using Quasar Play
     // to generate a QR code then on each dev (re)compilation
