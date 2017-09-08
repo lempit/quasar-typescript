0fdcb8cfdca77f2ab67265b45d89d8b0e5b53b7d
budiadiono <budiadiono@gmail.com>
2017-09-08 15:59:25 +0700

    router/index.ts

--- template/src/router/index.ts
+++ template/src/router/index.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Hello from '../components/Hello.vue'
+import Error404 from '../components/Error404.vue'
+
 
 Vue.use(VueRouter)
-
-function load (component) {
-  // '@' is aliased to src/components
-  return () => import(`@/${component}.vue`)
-}
 
 export default new VueRouter({
   /*
@@ -22,9 +20,9 @@ export default new VueRouter({
    */
 
   routes: [
-    { path: '/', component: load('Hello') },
+    { path: '/', component: Hello },
 
     // Always leave this last one
-    { path: '*', component: load('Error404') } // Not found
+    { path: '*', component: Error404 } // Not found
   ]
 })
